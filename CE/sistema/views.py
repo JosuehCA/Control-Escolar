2e8c14bafea562b3cd8e48858d4dfc8bdb6f12a8
JosuehCA <josuemago2012@gmail.com>
2024-10-08 12:27:12 -0600

    Login/logout añadido. Paquetes de vistas y urls actualizados.

--- CE/sistema/views.py
+++ CE/sistema/views.py
@@ -1,3 +1,64 @@
 from django.shortcuts import render
+from django import forms
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect
+
+from .models import UsuarioEscolar
 
 # Create your views here.
+
+def index(request):
+    return render(request, "sistema/index.html")
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "sistema/login.html", {
+                "message": "Nombre de usuario o contraseña incorrectos."
+            })
+    else:
+        return render(request, "sistema/login.html")
+    
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]   
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "sistema/register.html", {
+                "message": "Las contraseñas no son iguales."
+            })
+
+        # Attempt to create new user
+        try:
+            user = UsuarioEscolar.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "sistema/register.html", {
+                "message": "Nombre de usuario ocupado."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "sistema/register.html")
